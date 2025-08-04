f7c8b696bfb331470eab8d0a716ac46022e645c4
Shlok <22cs086@charusat.edu.in>
2025-08-04 14:20:05 +0530

    designin1.

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
+import TextareaAutosize from "react-textarea-autosize";
 
 export default function ContactUs() {
   return (
