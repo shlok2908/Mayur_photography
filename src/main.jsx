7a7bdd16ac39a824716ac595b72ccdc8e312c2fd
Shlok <22cs086@charusat.edu.in>
2025-08-05 19:00:37 +0530

    ./

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/global.css';
-import { LoaderProvider } from "./context/LoaderContext";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <LoaderProvider>
       <App />
-    </LoaderProvider>
   </BrowserRouter>
 );
